eb0eb9e8d8efab2bdc367e3810347482a3a75c91
smitl <smitl@DESKTOP-D558B6Q>
2023-02-05 21:06:32 -0500

    Add project files.

---
+++ Library.Management/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Management
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Dictionary<int, double> Grades { get; set; }
+        public char Classification { get; set; }
+        public Person()
+        {
+            Name =
+            Grades = new Dictionary<int, double>();
+        }
+    }
+}  
